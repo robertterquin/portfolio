77cbcac33fd1a094214f94e3eb90295ec74438a0
Robert Terquin Laqui <robertterquin.laqui@sdca.edu.ph>
2025-02-26 08:31:05 +0800

    Initial Commit

---
+++ vanta.js
@@ -0,0 +1,14 @@
+VANTA.NET({
+  el: "#vanta-bg",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  color: 0xffffff,
+  backgroundColor: 0x90909,
+  spacing: 20.00,
+  showDots: false
+})
