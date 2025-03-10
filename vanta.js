f48e176af5a6530e8f487ca209eaee6b17717f8d
Robert Terquin Laqui <terquinlaqui@gmail.com>
2025-03-10 22:50:13 +0800

    Updates

--- vanta.js
+++ vanta.js
@@ -1,14 +1,16 @@
-VANTA.NET({
-  el: "#vanta-bg",
-  mouseControls: true,
-  touchControls: true,
-  gyroControls: false,
-  minHeight: 200.00,
-  minWidth: 200.00,
-  scale: 1.00,
-  scaleMobile: 1.00,
-  color: 0xffffff,
-  backgroundColor: 0x90909,
-  spacing: 20.00,
-  showDots: false
-})
+document.addEventListener("DOMContentLoaded", function () {
+  VANTA.NET({
+    el: "#vanta-bg",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color: 0xffffff,
+    backgroundColor: 0x090909, // Make sure this is correct
+    spacing: 20.00,
+    showDots: false
+  });
+});
