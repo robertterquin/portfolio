a6f59aee9e63fd15872378a31078cd58bc57e644
Robert Terquin Laqui <terquinlaqui@gmail.com>
2025-03-24 12:45:17 +0800

    Updates

---
+++ transition.js
@@ -0,0 +1,10 @@
+gsap.from("body", { opacity: 0, duration: 1 });
+document.querySelectorAll("a.nav-link").forEach(link => {
+    link.addEventListener("click", function (event) {
+        event.preventDefault();
+        const targetUrl = this.href;
+        gsap.to("body", { opacity: 0, duration: 0.5, onComplete: () => {
+            window.location.href = targetUrl;
+        }});
+    });
+});
